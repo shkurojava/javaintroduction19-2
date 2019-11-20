2600d4eca534b247956163d1ac873b3f7e914a7d
Администратор <@PC>
2019-11-21 01:55:16 +0200

    Home work is Done

--- HomeTaskV2/src/ua/dp/ort/homework2/HomeWork2.java
+++ HomeTaskV2/src/ua/dp/ort/homework2/HomeWork2.java
@@ -4,7 +4,7 @@ public class HomeWork2 {
 
 	public static void main(String[] args) {
       System.out.println("Home work is almost done");
-      System.out.println("Home work is done");
+      System.out.println("Home work is Done");
 	}
 
 }
