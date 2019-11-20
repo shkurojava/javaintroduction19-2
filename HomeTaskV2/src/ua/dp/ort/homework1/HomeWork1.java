f43b291082d116ae6973b28ca2fc91a453cfc34a
Администратор <@PC>
2019-11-21 00:58:00 +0200

    Add home work 1

---
+++ HomeTaskV2/src/ua/dp/ort/homework1/HomeWork1.java
@@ -0,0 +1,9 @@
+package ua.dp.ort.homework1;
+
+public class HomeWork1 {
+
+	public static void main(String[] args) {
+		System.out.println("      *       \"\"\"     \\          /    ```" + "\n" + "      *      \"   \"     \\        /    `   `" + "\n" + "      *     \"     \"     \\      /    `     `" + "\n" + "      *     \"\"\"\"\"\"\"      \\    /     ```````" + "\n" + "  *   *     \"     \"       \\  /      `     `" + "\n" + "   ***      \"     \"        \\/       `     `");
+	}
+
+}
